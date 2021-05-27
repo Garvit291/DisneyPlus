455190a49ac0346077147d6772e2de816e973316
Garvit291 <chhabragarvit291@outlook.com>
2021-05-28 01:50:43 +0530

    header added

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,13 +1,6 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  incrementIfOdd,
-  selectCount,
-} from './counterSlice';
+
 import styles from './Counter.module.css';
 
 export function Counter() {
