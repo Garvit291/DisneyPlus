448ea3901c58c9e314fa650fdd8907a10d9d212f
Garvit291 <chhabragarvit291@outlook.com>
2021-05-31 02:22:33 +0530

    Detail page completed

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
+import play from './images/play-icon-black.png' ;
+import trailer from './images/play-icon-white.png' ;
+import group from './images/group-icon.png' ;
+
+
+
 
 function Detail() {
     return (
@@ -12,18 +18,29 @@ function Detail() {
            </ImageTitle>
            <Controls>
                <PlayButton>
-
+                    <img src={play}/>
+                    <span>PLAY</span>
                </PlayButton>
                <TrailerButton>
-
+                    <img src={trailer}/>
+                    <span>TRAILER</span>
                </TrailerButton>
                <AddButton>
-
+                    <span>+</span>
                </AddButton>
                <GroupWatchButton>
-
+                    <img src={group}/>
                </GroupWatchButton>
            </Controls>
+               
+           <Subtitle>
+           2018 · 7m · Family , Kids , Animation
+           </Subtitle>
+           <Description>
+                This is  the Description, This is  the DescriptionThis is  the DescriptionThis is  the DescriptionThis is  the Description
+                This is  the DescriptionThis is  the DescriptionThis is  the DescriptionThis is  the DescriptionThis is  the DescriptionThis is  the Descriptio
+                This is  the Description
+           </Description>
         </Container>
     )
 }
@@ -67,19 +84,66 @@ const ImageTitle = styled.div`
 `
 
 const Controls = styled.div`
+    display:flex;
+    align-items:center;
 
 `
 
-const PlayButton = styled.div `
+const PlayButton = styled.button `
+
+    border-radius:4px;
+    font-size:15px;
+    display:flex;
+    padding:0px 24px;
+    margin-right:22px;
+    align-items:center;
+    height:56px;
+    background:rgb(249,249,249);
+    border:none;
+    letter-spacing:1.8px;
+    cursor:pointer;
+
+    &:hover{
+        background:rgb(198,198,198);
+    }
 `
 
-const TrailerButton = styled.div `
-
+const TrailerButton = styled(PlayButton) `
+    background:rgba(0,0,0,0.3);
+    border: 1px solid rgb(249,249,249);
+    color: rgb(249,249,249);
 `
 
-const AddButton = styled.div `
+const AddButton = styled.button `
+    width:44px;
+    margin-right:16px;
+    height:44px;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    border-radius:50%;
+    border: 2px solid white;
+    background-color:rgba(0,0,0,0.6);
+    cursor:pointer;
+    span{
+        font-size:30px;
+        color:white;
+    }
 
 `
-const GroupWatchButton = styled.div `
+const GroupWatchButton = styled(AddButton)`
+        background:rgb(0,0,0);
+`
+const Subtitle = styled.div`
+    color:rgb(249,249,249);
+    font-sixe:15px;
+    min-height :20px;
+    margin-top :26px;
+`
+const Description = styled.div`
+    line-height :1.4;
+    font-size:20px;
+    margin-top:16px;
+    color:rgb(249,249,249);
 
 `
